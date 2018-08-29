57b4c47381cd1ca251ffe1532d57efd592aabe54
ultimatepig <theultimatepig@gmail.com>
2018-08-29 05:43:44 -0700

    initial commit

---
+++ client/src/js/View.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class View extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.id,
+      status: '???',
+      queue: ['???'],
+      matches: ['???']
+    };
+
+    props.socket.emit('get view', props.id);
+
+    props.socket.on('update view', function(data) {
+      console.log('updating!');
+      if (this.mounted) {
+        this.setState({status: data.status,
+                       queue: data.queue,
+                       matches: data.matches});
+      }
+    }.bind(this));
+  }
+
+    componentDidMount() {
+      this.mounted = true;
+    }
+
+    componentWillUnmount() {
+      this.mounted = false;
+    }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.id}</p>
+        <p>{this.state.status}</p>
+        <p>{this.state.queue}</p>
+        <p>{this.state.matches}</p>
+      </div>
+    )
+  }
+}
+
+export default View;
